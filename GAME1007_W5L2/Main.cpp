e3b3a6555b989a5b01d6ca1d8fb785ef3428037e
Mueez Sabir <MueezSabir2002@gmail.com>
2021-10-06 10:01:59 -0400

    GAME1007-W5L2-SabirMueez

--- GAME1007_W5L2/Main.cpp
+++ GAME1007_W5L2/Main.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
+using namespace std;
+
+// Init Function() Sets up SDL and all subsystems. 
+void Init() 
+{
+	cout << "Initializing game..." << endl; 
+}
 
 
+// Run function. Contains the main game loop.
+int Run()
+{
+	bool isRunning = true; 
+	char input;
+
+
+	Init();
+	while (isRunning) // This is our game loop. 
+	{
+
+	}
+
+	return 0;
+}
+
+
+// main function. Entry point of C++ program. 
 int main()
 {
 
-
-
-
-
-
-
-
-
-
-	return 0;
+	return Run();
 }
